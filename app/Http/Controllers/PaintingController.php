84989aa69fdd579cbb317f4f5cc3a600e072d340
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-11-26 16:57:16 +0100

    cardok

--- app/Http/Controllers/PaintingController.php
+++ app/Http/Controllers/PaintingController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PaintingController extends Controller
 {
+    private $paintings = [];
+    function __construct()
+    {
+        $this->paintings = Storage::json('Painting.json');
+    }
+
     public function index()
     {
-        return view('index');
+        return view('index', ['paintings' => $this->paintings]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,14 @@
 <x-layout>
-    <h1>Teszt</h1>
+    @foreach ($paintings as $paint  )
+        <div class="card" style="width: 18rem;">
+            <img src="{{$paint['Image']}}" class="card-img-top" alt="{{$paint['Painting']}}">
+            <div class="card-body">
+            <h5 class="card-title">{{$paint['Painting']}}</h5>
+            <h6 class="card-subtitle">Artist: {{$paint['Artist']}}</h6>
+            <p class="card-text">{{$paint['Description']}}</p>
+            <a href="/paintings/{{$paint['Painting']}}" class="btn btn-primary">Details</a>
+            </div>
+        </div>
+    @endforeach
+
 </x-layout>
